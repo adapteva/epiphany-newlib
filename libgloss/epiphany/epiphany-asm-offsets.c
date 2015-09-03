0758898e199e45fb6392f7fb88e9decd244ad0eb
Ola Jeppsson <ola@adapteva.com>
2015-09-03 17:29:58 +0200

    epiphany: Don't use hardcoded struct offsets
    libgloss:
    	* epiphany/Makefile.in: Add rule for epiphany-asm-offsets.h
    	* epiphany/crt0.S: Don't use hard coded struct offsets.
    	* epiphany/epiphany-asm-offsets.c: Make output parser-friendly.
    	Fix compiler warnings.
    
    Signed-off-by: Ola Jeppsson <ola@adapteva.com>

--- libgloss/epiphany/epiphany-asm-offsets.c
+++ libgloss/epiphany/epiphany-asm-offsets.c
@@ -1,16 +1,17 @@
 /* Idea from: http://blackfin.uclinux.org/doku.php?id=toolchain:gas:structs */
+#include <stddef.h>
 #include <sys/reent.h>
 
 #undef offsetof
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 #define SIZEOF(TYPE, MEMBER) (sizeof(((TYPE *)0)->MEMBER))
 
-#define _DEFINE(sym, val) asm("\n-> " #sym " %0 " #val "\n" : : "i" (val))
+#define _DEFINE(sym, val) asm("\n#define " #sym " %0\n" : : "i" (val))
 #define DEFINE(s, m) \
 		_DEFINE(offsetof_##s##_##m, offsetof(s, m)); \
 	_DEFINE(sizeof_##s##_##m, SIZEOF(s, m));
 
-main()
+int main()
 {
 	typedef struct _reent _reent;
 	typedef struct _atexit _atexit;
@@ -19,4 +20,5 @@ main()
 	DEFINE(_atexit, _ind)
 	DEFINE(_atexit, _fns);
 
+	return 0;
 }
